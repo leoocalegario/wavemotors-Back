013d5ceacfe140b02bf7c4e955a67a8201e10d8b
Leonardo Calegario <104872263+leoocalegario@users.noreply.github.com>
2024-04-16 13:45:55 -0300

    update anuncio_veiculo

--- src/main/java/app/controller/AnuncioVeiculoController.java
+++ src/main/java/app/controller/AnuncioVeiculoController.java
@@ -1,8 +1,98 @@
 package app.controller;
 
-import org.springframework.validation.annotation.Validated;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.AnuncioVeiculo;
+import app.service.AnuncioVeiculoService;
+
+@RestController
+@RequestMapping("/api/anuncioveiculo")
 @Validated
 public class AnuncioVeiculoController {
+	
+	@Autowired
+	private AnuncioVeiculoService anuncioveiculoservice;
+	
+	@PostMapping("save")
+	public ResponseEntity<String> save (@RequestBody AnuncioVeiculo anuncioveiculo){
+		try {
+			String mensagem = this.anuncioveiculoservice.save(anuncioveiculo);
+			return new ResponseEntity<String>(mensagem, HttpStatus.OK);
+		} catch (Exception e) {
+			
+			return new ResponseEntity<String>("erro: " +e.getMessage(),HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PutMapping("/update/{idAnuncio}")
+	public ResponseEntity<String>update (@RequestBody AnuncioVeiculo anuncioveiculo, @PathVariable Long idAnuncio ){
+		try {
+			String mensagem = this.anuncioveiculoservice.update(idAnuncio, anuncioveiculo);
+			return new ResponseEntity<String>(mensagem,HttpStatus.OK);
+		} catch (Exception e) {
+			
+			return new ResponseEntity<String>("erro: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping("/listAll")
+	public ResponseEntity<List<AnuncioVeiculo>>listAll(){
+		try {
+			List<AnuncioVeiculo> lista = this.anuncioveiculoservice.listAll();
+			return new ResponseEntity<>(lista,HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+			
+		}
+	}
+	
+	@GetMapping("/findById/{idAnuncio}")
+	public ResponseEntity<AnuncioVeiculo> findById(@PathVariable Long idAnuncio){
+		try {
+			AnuncioVeiculo anuncioveiculo = this.anuncioveiculoservice.findById(idAnuncio);
+			return new ResponseEntity<>(anuncioveiculo,HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/delete/{idAnuncio}")
+	public ResponseEntity<String> delete(@PathVariable Long idAnuncio) {
+
+		try {
+
+			String mensagem = this.anuncioveiculoservice.delete(idAnuncio);
+			return new ResponseEntity<>(mensagem, HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Deu esse erro aqui: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+	
+	@GetMapping("/findByNomeLike")
+	public ResponseEntity<List<AnuncioVeiculo>> findByNomeLike(@RequestParam String nomeveiculo) {
+	    try {
+	        List<AnuncioVeiculo> anuncioveiculo = this.anuncioveiculoservice.findByNomeLike(nomeveiculo);
+	        return new ResponseEntity<>(anuncioveiculo, HttpStatus.OK);
+	    } catch (Exception e) {
+	        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+	    }
+	}
+
 
 }
--- src/main/java/app/service/AnuncioVeiculoService.java
+++ src/main/java/app/service/AnuncioVeiculoService.java
@@ -1,5 +1,49 @@
 package app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.AnuncioVeiculo;
+import app.repository.AnuncioVeiculoRepository;
+
+@Service
 public class AnuncioVeiculoService {
+	
+	@Autowired
+	private AnuncioVeiculoRepository anuncioveiculorepository;
+
+	public String save (AnuncioVeiculo anuncioveiculo) {
+		
+		this.anuncioveiculorepository.save(anuncioveiculo);
+		return anuncioveiculo.getNomeVeiculo() + " salvo com sucesso";
+	}
+	
+	public String update (Long idAnuncio, AnuncioVeiculo anuncioveiculo ) {
+		anuncioveiculo.setIdAnuncio(idAnuncio);
+		this.anuncioveiculorepository.save(anuncioveiculo);
+		return anuncioveiculo.getNomeVeiculo() + "atualizado com sucesso";
+	}
+	
+	public String delete(Long idAnuncio) {
+		this.anuncioveiculorepository.deleteById(idAnuncio);
+		return "Anuncio deletado com sucesso";
+	}
+	
+	public List<AnuncioVeiculo> listAll(){
+		return this.anuncioveiculorepository.findAll();
+	}
+	
+	public AnuncioVeiculo findById(Long id) {
+		AnuncioVeiculo anuncioveiculo = this.anuncioveiculorepository.findById(id).get();
+		return anuncioveiculo;
+	}
+	
+	public List<AnuncioVeiculo> findByNomeLike(String nomeveiculo) {
+		List<AnuncioVeiculo> anuncioveiculo = anuncioveiculorepository.findByNomeLike(nomeveiculo);
+		return anuncioveiculo;
+	}
+	
 
 }
